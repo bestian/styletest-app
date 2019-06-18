7096977ddc169fefcc314ed89e4a0e79db4d7bf7
Bestian Tang <bestian@gmail.com>
2019-06-18 21:58:10 +0800

    Revert "init"
    This reverts commit f0d731b6bbd0919a803258f9bb847fb6e9f91927.

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,5 @@ module.exports = {
   publicPath: '',
   pluginOptions: {
     cordovaPath: 'src-cordova'
-  },
-  chainWebpack: config => {
-      config.module.rule('pug')
-          .test(/\.pug$/)
-          .use('pug-html-loader')
-          .loader('pug-html-loader')
-          .end()
   }
 }
